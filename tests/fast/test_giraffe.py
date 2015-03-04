4ab47a81b7f0aa12047542d9106d8609cca3dc62
Jerome <jerome.thai.87@gmail.com>
2015-03-03 16:38:53 -0800

    fix import in test_giraffe

--- tests/fast/test_giraffe.py
+++ tests/fast/test_giraffe.py
@@ -15,7 +15,7 @@ class TestGiraffe(unittest.TestCase):
 
     def test_height(self):
         # This tests that the random number is 4
-        from boilerplate.Giraffe import Giraffe 
+        from boilerplate.giraffe import Giraffe 
         g = Giraffe()
         self.assertTrue(g.height == 7)
 
